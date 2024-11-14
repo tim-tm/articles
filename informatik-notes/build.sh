a1416fb85101c8f3aceb2662ed4ffb4dea8e2ae4
Tim Teichmann <teichmanntim@outlook.de>
2024-11-14 18:59:36 +0100

    add: informatik-notes, mathe-notes

---
+++ informatik-notes/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+mkdirs() {
+    mkdir -p build
+    mkdir -p build/includes
+}
+
+update_bib() {
+    cp refs.bib build
+    cd build
+    bibtex informatiknotes
+    cd ..
+}
+
+build_pdf() {   
+    pdflatex -aux-directory=build -output-directory=build informatiknotes.tex
+}
+
+set -xe
+
+mkdirs
+build_pdf
+update_bib
+build_pdf
